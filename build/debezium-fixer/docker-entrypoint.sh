f5b64b59049796875ae2d534d48003c9bb583721
gandharv <gandharv.suri@iiitb.org>
2021-12-07 13:08:54 +0530

    Edit: Debezium fixer entrypoint

--- build/debezium-fixer/docker-entrypoint.sh
+++ build/debezium-fixer/docker-entrypoint.sh
@@ -10,7 +10,9 @@ while true; do
   for i in $(curl -s "http://$MY_POD_IP:8083/connectors" | jq -r '.[]'); do
     TASK_COUNT=$(curl -s "http://$MY_POD_IP:8083/connectors/$i/tasks" | jq length)
     for j in $(seq 0 $((TASK_COUNT-1))); do
-      if ! [ -z "$(curl -s \"http://$MY_POD_IP:8083/connectors/$i/tasks/$j/status\" | grep -i $ERROR_LEVEL1 | grep -i $ERROR_LEVEL2)" ]; then
+      svr=$(curl -s "http://$MY_POD_IP:8083/connectors/$i/tasks/$j/status")
+      svr=$(echo $svr | grep -i "$ERROR_LEVEL1" | grep -i "$ERROR_LEVEL2")
+      if ! [ -z "$svr" ]; then
         echo "$(date) [ERROR]: Ran into the given error $ERROR_LEVEL1 -> $ERROR_LEVEL2; in batch number $which_batch; in the connector $i; in task number $j. Restarting ..."
         curl -XPOST -s "http://$MY_POD_IP:8083/connectors/$i/tasks/$j/restart" 
       fi
